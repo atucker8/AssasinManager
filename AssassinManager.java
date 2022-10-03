30587106ffbbe2c63ccfe0b15bd919770982763f
tuckeam <tuckeam@students.mtsd.k12.wi.us>
2022-10-02 21:58:59 -0500

    1

---
+++ AssassinManager.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class AssassinManager {
+   private AssassinNode front=new AssassinNode(null);
+   private AssassinNode graveyard=new AssassinNode(null);
+
+public AssassinManager(List<String> names){
+    if(names.size()==0){
+        throw new IllegalArgumentException();
+    }
+  
+    }
+
+    public void printKillRing(){
+        
+    }
+
+    public void printGraveyard(){
+       
+    }
+
+
+}
+}
