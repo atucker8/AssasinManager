b50d9565a9c3c0d60e12b71d90c862c53de3cc1d
tuckeam <tuckeam@students.mtsd.k12.wi.us>
2022-10-03 21:59:21 -0500

    almost not rly

--- AssassinManager.java
+++ AssassinManager.java
@@ -4,7 +4,8 @@ public class AssassinManager {
    private AssassinNode front;
    private AssassinNode graveyard;
    private AssassinNode current;
-   private int size=0;
+   private int sizeK=0;
+   private int sizeG=0;
 public AssassinManager(List<String> names){
     front=new AssassinNode(names.get(0));
     graveyard= new AssassinNode(null);
@@ -14,25 +15,81 @@ public AssassinManager(List<String> names){
     }
         for(int i=1;i<=names.size();i++){
             front.next=new AssassinNode(names.get(i));
-            size++;
+            sizeK++;
         }
     }
 
     public void printKillRing(){
-        if(size==1){
+        if(sizeK==1){
             System.out.print(front+" is stalking "+front);
         }
-        for(int i=0;i<size;i++){
-            System.out.print(current);
+        for(int i=0;i<sizeG;i++){
+            System.out.print("    "+current.name);
             current=current.next;
-         System.out.println(" is stalking "+current);
+         System.out.println(" is stalking "+current.name);
         }
     }
 
     public void printGraveyard(){
+        current=graveyard;
+        for(int i=0;i<sizeG;i++){
+            System.out.print("    "+current.name);
+            current=current.next;
+            System.out.println(" was killed by "+current.name);
+        }
        
     }
 
+    public boolean killRingContains(String name){
+        boolean yn=false;
+        current=front;
+        for(int i=0;i<sizeK;i++){
+            if(current.name.toLowerCase().equals(name.toLowerCase())){
+                yn=true;
+            }
+            current=current.next;
+        }
+        return yn;
+    }
+    
+    public boolean graveyardContains(String name){
+        boolean yn=false;
+        current=graveyard;
+        for(int i=0;i<sizeG;i++){
+            if(current.name.toLowerCase().equals(name.toLowerCase())){
+                yn=true;
+            }
+        }
+        return yn;
+    }
 
-}
+    public boolean gameOver(){
+        return (sizeK==1);
+    }
+
+    public String winner(){
+        if(sizeK==1){
+            return front.name;
+        }
+        return null;
+    }
+
+    public void kill(String name){
+        if((gameOver()==true&&killRingContains(name)==false)||(gameOver()==true)){
+            throw new IllegalStateException();
+        }
+        if(killRingContains(name)==false){
+            throw new IllegalArgumentException();
+        }
+        AssassinNode temp=new AssassinNode(name);
+        temp.next=graveyard;
+        graveyard=temp;
+            
+    }
+
+
+
+
+
+    //sizeK-- and sizeG++ in kill method
 }
