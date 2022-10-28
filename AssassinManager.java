9334c76bd7e54235c3528362d32269cd3db3ffb7
tuckeam <tuckeam@students.mtsd.k12.wi.us>
2022-10-27 21:10:36 -0500

    s

--- AssassinManager.java
+++ AssassinManager.java
@@ -5,7 +5,7 @@ public class AssassinManager {
    private AssassinNode graveyard;
    private AssassinNode current;
    private int sizeK=0;
-   private int sizeG=0;
+//Contructor where kill ring is initialized from the list of names
 public AssassinManager(List<String> names){
     front=new AssassinNode(names.get(0));
     graveyard= new AssassinNode(null);
@@ -20,32 +20,32 @@ public AssassinManager(List<String> names){
         }
         current=front;
     }
-
+//prints the kill ring
     public void printKillRing(){
         current=front;
         if(sizeK==1){
             System.out.print(front+" is stalking "+front);
         }
-        for(int i=0;i<sizeK;i++){
+        while(current.next!=null){
             System.out.print("    "+current.name);
             current=current.next;
          System.out.println(" is stalking "+current.name);
         }
     }
-
+//prints the graveyard of people that were killed
     public void printGraveyard(){
         current=graveyard;
-        for(int i=0;i<sizeG;i++){
+        while(current.next!=null){
             System.out.println("    "+current.name+" was killed by "+current.killer);
             current=current.next;
         }
        
     }
-
+//checks if the kill ring still has a specific names and returns true if that person is there
     public boolean killRingContains(String name){
         boolean yn=false;
         current=front;
-        for(int i=0;i<sizeK;i++){
+        while(current.next!=null){
             if(current.name.toLowerCase().equals(name.toLowerCase())){
                 yn=true;
             }
@@ -53,29 +53,29 @@ public AssassinManager(List<String> names){
         }
         return yn;
     }
-    
+//checks if the graveyard has a name and returns true if that person is there
     public boolean graveyardContains(String name){
         boolean yn=false;
         current=graveyard;
-        for(int i=0;i<sizeG;i++){
+        while(current.next!=null){
             if(current.name.toLowerCase().equals(name.toLowerCase())){
                 yn=true;
             }
         }
         return yn;
     }
-
+//returns true of the game is over and false if there is more than one player
     public boolean gameOver(){
-        return (sizeK==1);
+        return (front.next==null);
     }
 
     public String winner(){
-        if(sizeK==1){
+        if(gameOver()==true){
             return front.name;
         }
         return null;
     }
-
+//kills the person and assigns their killer to whoever that person was stalking 
     public void kill(String name){
         if((gameOver()==true&&killRingContains(name)==false)||(gameOver()==true)){
             throw new IllegalStateException();
@@ -85,11 +85,9 @@ public AssassinManager(List<String> names){
         }
         AssassinNode temp;
         current=front;
-        for(int i=0;i<sizeK-1;i++){
+        while(current.next!=null){
             if(current.next.name.toLowerCase().equals(name.toLowerCase())){
                 current.next.killer=current.name;
-                sizeK--;
-                sizeG++;
                 temp=current.next;
                 current.next=current.next.next;
                 temp.next=graveyard;
